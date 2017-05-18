6071cebeae474bd2905b75a3ea282a6ccc427351
Robert Byrne <bobbyrne01@gmail.com>
2017-05-18 17:33:14 +0100

    using expressions

--- js/templatingLogic.js
+++ js/templatingLogic.js
@@ -1,19 +1,21 @@
-var templateScript = document.getElementById('address-template').innerHTML.trim();
-console.log(templateScript);
-
-// Compile the template
-var template = Handlebars.compile(templateScript);
-console.log(template);
-
-// Define our data object
-var context = {
+var addressTemplateScript = document.getElementById('address-template').innerHTML.trim();
+var addressTemplate = Handlebars.compile(addressTemplateScript);
+var addressContext = {
   "city": "Dublin",
   "street": "River road",
   "number": "1"
 };
+var addressCompiledHtml = addressTemplate(addressContext);
+document.getElementById('address-content-placeholder').innerHTML = addressCompiledHtml;
 
-// Pass data to the template
-var compiledHtml = template(context);
-
-// Add the compiled html to the page
-document.getElementById('content-placeholder').innerHTML = compiledHtml;
+var expressionsTemplateScript = document.getElementById('expressions-template').innerHTML.trim();
+var expressionsTemplate = Handlebars.compile(expressionsTemplateScript);
+var expressionsContext = {
+  "description": {
+    "escaped": "Using {{}} brackets will result in escaped HTML:",
+    "unescaped": "Using {{{}}} will leave the context as it is:"
+  },
+  "example": "<button> Hello </button>"
+};
+var expressionsCompiledHtml = expressionsTemplate(expressionsContext);
+document.getElementById('expressions-content-placeholder').innerHTML = expressionsCompiledHtml;
